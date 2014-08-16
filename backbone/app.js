98a9aa12a9cb0c35c2908f805ff5d4cef02d4f9e
island205 <island205@gmail.com>
2014-08-16 23:53:44 +0800

    start backbone mvc

---
+++ backbone/app.js
@@ -0,0 +1,12 @@
+require('./bower_components/backbone.localStorage/backbone.localStorage');
+var AppView = require('./views/app-view');
+var ENTER_KEY = 13;
+var ESC_KEY = 27;
+var $ = require('jquery');
+
+$(function () {
+  'use strict';
+
+  // kick things off by creating the `App`
+  new AppView();
+});
---
+++ backbone/backbone.js
@@ -0,0 +1,5 @@
+var Backbone = require('backbone')
+var $ = require('jquery')
+Backbone.$ = $
+
+module.exports = Backbone
