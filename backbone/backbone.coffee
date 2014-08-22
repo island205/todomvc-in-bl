fa0b80b0c65d74a153cdba4e9bb1efe531d2e84c
island205 <island205@gmail.com>
2014-08-23 00:19:06 +0800

    coffee-script example

---
+++ backbone/backbone.coffee
@@ -0,0 +1,6 @@
+Backbone = require 'backbone'
+$ = require 'jquery'
+
+Backbone.$ = $
+
+module.exports = Backbone
--- backbone/backbone.js
+++
@@ -1,5 +0,0 @@
-var Backbone = require('backbone')
-var $ = require('jquery')
-Backbone.$ = $
-
-module.exports = Backbone
